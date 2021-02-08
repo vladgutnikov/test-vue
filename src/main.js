a200f94b8b44004e0b44df8d296d06b994aa5f3a
Vlad <vladyslav.gutnikov.work@gmail.com>
2021-02-08 16:14:18 +0200

    starting project setup

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+
 import App from './App.vue';
 
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
 
 new Vue({
+  vuetify: new Vuetify(),
   render: (h) => h(App),
 }).$mount('#app');
