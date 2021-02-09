f280c3e4dafd6a78c45274642957907d1eaa2282
Vlad <vladyslav.gutnikov.work@gmail.com>
2021-02-09 09:38:31 +0200

    added user filtering function

--- src/components/Headers.vue
+++ src/components/Headers.vue
@@ -1,42 +1,65 @@
 <template>
-  <v-bottom-navigation dark shift height="auto">
-    <v-container>
-      <v-row class="justify-center">
-        <v-col md="3">
-          <div class="wrapper-search">
-            <v-text-field
-              class="my-search"
-              v-model="search"
-              label="Search"
-              outlined
-              @input="getUserData"
-              prepend-inner-icon="search"
-            ></v-text-field>
-          </div>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-bottom-navigation>
+  <div>
+    <v-bottom-navigation shift height="auto">
+      <v-container>
+        <v-row class="justify-center">
+          <v-col md="4">
+            <div class="wrapper-search">
+              <v-text-field
+                class="my-search"
+                v-model="search"
+                label="Search"
+                outlined
+                @input="getUserData"
+                @blur="clearDataArray"
+                prepend-inner-icon="search"
+              ></v-text-field>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-bottom-navigation>
+    <ListItems :items="filterData" />
+  </div>
 </template>
 
 <script>
+import ListItems from './ListItems';
+import data from '../data/data.json';
+
 export default {
-  data: () => ({ search: null }),
-  computed: {},
+  data: () => ({ search: null, items: [], filterData: [] }),
+  components: {
+    ListItems,
+  },
   methods: {
     getUserData() {
-      console.log(this.search);
+      this.filterData = this.items.filter((item) => {
+        if (item.title.toUpperCase().indexOf(this.search.toUpperCase()) > -1)
+          return item;
+      });
     },
+    clearDataArray() {
+      if (!this.search) this.filterData = [];
+    },
+  },
+  mounted() {
+    this.items = [...data];
   },
 };
 </script>
 
-<style>
+<style lang="scss">
 .wrapper-search {
   position: relative;
+
+  .my-search {
+    color: #000;
+  }
 }
 .my-search {
   background: #ccc;
   height: 56px;
+  color: #000;
 }
 </style>
---
+++ src/components/ListItems.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container>
+    <v-row class="justify-center">
+      <v-col md="4">
+        <v-card max-width="100%" class="mx-auto" v-show="!!items.length">
+          <v-list three-line>
+            <template v-for="(item, index) in items">
+              <v-subheader
+                v-if="item.header"
+                :key="item.header"
+                v-text="item.header"
+              ></v-subheader>
+
+              <v-divider
+                v-else-if="item.divider"
+                :key="index"
+                :inset="item.inset"
+              ></v-divider>
+
+              <v-list-item v-else :key="item.title">
+                <v-list-item-avatar>
+                  <v-img :src="item.avatar"></v-img>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                  <v-list-item-title v-html="item.title"></v-list-item-title>
+                  <v-list-item-subtitle
+                    v-html="item.subtitle"
+                  ></v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </template>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+    },
+  },
+};
+</script>
